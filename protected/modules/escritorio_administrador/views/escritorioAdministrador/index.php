a995aa749ca7a5d2c375bc2bf0cb9b96744ab7bf
john lopez suarez <jlopez@utemvirtual.cl>
2015-07-23 18:16:00 -0300

    ADD: Trabajando en el escritorio de Metro UI

--- protected/modules/escritorio_administrador/views/escritorioAdministrador/index.php
+++ protected/modules/escritorio_administrador/views/escritorioAdministrador/index.php
@@ -1,13 +1,10 @@
-   
 <?php $dataArray = $dataProvider->getData();?>
-
+<div class="tile-group double">
     <span class="tile-group-title">Other</span>
-    <div class="tile-container">
-
+    <div class="tile-container ">
         <?php foreach ($dataArray as $data):?>
-
         <a href="<?php echo 'http://localhost/testrekoadministracion/'.$data->name;?>" >
-            <div class="tile bg-red fg-white" data-role="tile">   
+            <div class="tile-wide bg-red fg-white" data-role="tile">   
                 <div class="tile-content iconic" >                
                     <span class="icon mif-skype"></span>
                 </div>
@@ -22,25 +19,10 @@
                 </span>
             </div>
         </a>
-        <div class="tile bg-darkBlue fg-white" data-role="tile">
-            <div class="tile-content iconic">
-                <span class="icon mif-cloud"></span>
-            </div>
-            <span class="tile-label">OneDrive</span>
-        </div>
-        <div class="tile" data-role="tile" data-role="tile" data-effect="slideUpDown">
-            <div class="tile-content">
-                <div class="live-slide"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/me.jpg" data-role="fitImage" data-format="fill"></div>
-                <div class="live-slide"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/spface.jpg" data-role="fitImage" data-format="fill"></div>
-                <div class="live-slide"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/jeki_chan.jpg"></div>
-            </div>
-            <div class="tile-label">Photos</div>
-        </div>
         
         <?php endforeach;?>
-
-
     </div>
+</div>
 
 
                   
