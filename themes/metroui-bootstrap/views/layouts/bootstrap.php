fbd1469d4c484024b5e446f09bbbab21f45d63ac
john lopez suarez <jlopez@utemvirtual.cl>
2015-07-29 18:26:35 -0300

    ADD: Icono

--- themes/metroui-bootstrap/views/layouts/bootstrap.php
+++ themes/metroui-bootstrap/views/layouts/bootstrap.php
@@ -37,8 +37,27 @@
 		)); ?><!-- breadcrumbs -->
 	<?php endif?>
 
-	<?php echo $content; ?>
-
+<div class="row">
+    <div class="span9">
+        <div id="content">
+            <?php echo $content; ?>
+        </div><!-- content -->
+    </div>
+    <div class="span3">
+        <div id="sidebar">
+        <?php
+            $this->beginWidget('zii.widgets.CPortlet', array(
+                'title'=>'Operations',
+            ));
+            $this->widget('bootstrap.widgets.TbMenu', array(
+                'items'=>$this->menu,
+                'htmlOptions'=>array('class'=>'operations'),
+            ));
+            $this->endWidget();
+        ?>
+        </div><!-- sidebar -->
+    </div>
+</div>
 	<div class="clear"></div>
 
 	<div id="footer">
