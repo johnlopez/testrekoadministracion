6943ef4b7d1632146c6221c34bb5be532c6e9d2a
john lopez suarez <jlopez@utemvirtual.cl>
2015-08-31 17:48:10 -0300

    ADD: Sincronizacion Terminada

--- base de datos/testreko.sql
+++ base de datos/testreko.sql
@@ -6,7 +6,7 @@
 -- Tiempo de generación: 31-08-2015 a las 17:38:23
 -- Versión del servidor: 5.5.20
 -- Versión de PHP: 5.3.10
-
+SET foreign_key_checks = 0;
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
