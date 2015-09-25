0d3eda88d054c4a8ee53d5057138541de3965d20
john lopez suarez <jlopez@utemvirtual.cl>
2015-09-25 18:11:33 -0300

    ADD: Tranajando en asignar rol_usuario a privilegio_usuario

---
+++ protected/modules/admin_rol_usuario/controllers/RolusuariohasprivilegiousuarioController.php
@@ -0,0 +1,40 @@
+<?php
+
+class RolusuariohasprivilegiousuarioController extends Controller
+{
+	public function actionIndex()
+	{
+            if(isset($_POST['authitem_permiso_usuario_name']))
+            {
+                echo $_POST['authitem_permiso_usuario_name'];
+            }                        
+            $this->render('index');
+	}
+
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
--- protected/runtime/gii-1.1.16/ModelCode.php
+++ protected/runtime/gii-1.1.16/ModelCode.php
@@ -3,11 +3,7 @@ return array (
   'template' => 'default',
   'connectionId' => 'db',
   'tablePrefix' => '',
-<<<<<<< HEAD
-  'modelPath' => 'application.modules.admin_curricular.models',
-=======
   'modelPath' => 'application.modules.admin_rol_usuario.models',
->>>>>>> 292ec135b9656ae0d667aee9a3933c63248fcda5
   'baseClass' => 'CActiveRecord',
   'buildRelations' => '1',
   'commentsAsLabels' => '0',
