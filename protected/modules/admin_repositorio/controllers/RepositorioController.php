cd155aa8b3c96ad31be5eefec953a678adad0c3f
john lopez suarez <jlopez@utemvirtual.cl>
2015-12-28 13:26:57 -0300

    ADD: Listo copia de archivos para un repositorio asignado a un modulo / arreglar detalles de terminos SLUG

--- protected/modules/admin_repositorio/controllers/RepositorioController.php
+++ protected/modules/admin_repositorio/controllers/RepositorioController.php
@@ -62,21 +62,33 @@ class RepositorioController extends Controller
 	 */
 	public function actionCreate()
 	{
-		$model=new Repositorio;
-
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
-
-		if(isset($_POST['Repositorio']))
-		{
-			$model->attributes=$_POST['Repositorio'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
-
-		$this->render('create',array(
-			'model'=>$model,
+                $repositorio = new Repositorio();
+                if(isset($_POST['Repositorio']))
+                {
+                    $repositorio->attributes = $_POST['Repositorio'];
+                    if($repositorio->agregarRepositorioTroncal($repositorio->nombre, $repositorio->descripcion))
+                    {
+                        $this->redirect(array('view','id'=>$repositorio->lastInsertRepositorioId));
+                    }
+                }
+                $this->render('create',array(
+			'model'=>$repositorio,
 		));
+//		$model=new Repositorio;
+//
+//		// Uncomment the following line if AJAX validation is needed
+//		// $this->performAjaxValidation($model);
+//
+//		if(isset($_POST['Repositorio']))
+//		{
+//			$model->attributes=$_POST['Repositorio'];
+//			if($model->save())
+//				$this->redirect(array('view','id'=>$model->id));
+//		}
+//
+//		$this->render('create',array(
+//			'model'=>$model,
+//		));
 	}
 
 	/**
